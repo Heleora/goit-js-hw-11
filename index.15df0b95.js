4a5ea05b7b15be8b3d59c26f4e72ec19ca31c229
Heleora <105947027+Heleora@users.noreply.github.com>
2022-11-19 14:49:05 +0000

    Deploying to gh-pages from @ Heleora/goit-js-hw-11@871111cb1e05d084f740bf0b55e39770348a2aac 🚀

---
+++ index.15df0b95.js
@@ -0,0 +1,2 @@
+document.querySelector("form"),document.querySelector("input"),document.querySelector(".search-button"),document.querySelector(".gallery"),document.querySelector('[data="likes"]'),document.querySelector('[data="views"]'),document.querySelector('[data="comments"]'),document.querySelector('[data="downloads"]'),document.querySelector(".load-more");
+//# sourceMappingURL=index.15df0b95.js.map
