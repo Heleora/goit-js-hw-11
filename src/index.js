871111cb1e05d084f740bf0b55e39770348a2aac
Elena Pastushenko <heleora_net@ukr.net>
2022-11-19 16:48:41 +0200

    Creates gallery markup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const searchFormRef = document.querySelector("form");
+const searchInputRef = document.querySelector("input");
+const searchBtnRef = document.querySelector(".search-button");
+
+const galleryRef = document.querySelector(".gallery");
+const likesRef = document.querySelector(`[data="likes"]`);
+const viewsRef = document.querySelector(`[data="views"]`);
+const commentsRef = document.querySelector(`[data="comments"]`);
+const downloadsRef = document.querySelector(`[data="downloads"]`);
+
+const loadMoreBtnRef = document.querySelector(".load-more");
