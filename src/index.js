1c04affc792584e0c4473657a62492866f705e88
Elena Pastushenko <heleora_net@ukr.net>
2022-11-20 15:48:10 +0200

    pagination is done

--- src/index.js
+++ src/index.js
@@ -2,49 +2,54 @@
 import Notiflix from 'notiflix';
 
 const searchFormRef = document.querySelector("form");
-// const searchInputRef = document.querySelector("input");
-// const searchBtnRef = document.querySelector(".search-button");
-
 const galleryRef = document.querySelector(".gallery");
-// const likesRef = document.querySelector(`[data="likes"]`);
-// const viewsRef = document.querySelector(`[data="views"]`);
-// const commentsRef = document.querySelector(`[data="comments"]`);
-// const downloadsRef = document.querySelector(`[data="downloads"]`);
-
 const loadMoreBtnRef = document.querySelector(".load-more");
 
+// loadMoreBtnRef.addEventListener("click", loadMore);
+
 const MY_KEY = "31431755-1c4852ed09ff5890501267879";
+let page = 1;
 
 searchFormRef.addEventListener("submit", searchImages);
 
 function searchImages(evt) {
     evt.preventDefault();
-
-fetchImages(evt);
-// renderImages();
+    fetchImages(evt);
 };
 
 function fetchImages(evt) {
 const searchRequest = evt.currentTarget.elements.searchQuery.value;
-console.log(searchRequest);
-fetch(`https://pixabay.com/api/?key=${MY_KEY}&q=${searchRequest}&image_type=photo$orientation=horizontal&safesearch=true`)
+// console.log(searchRequest);
+fetch(`https://pixabay.com/api/?key=${MY_KEY}&per_page=4&q=${searchRequest}&image_type=photo$orientation=horizontal&safesearch=true`)
 .then(response => response.json())
 .then(data => {
-    console.log(data.hits[0]);
+    // console.log(data.hits[0]);
 
     if (data.hits.length === 0) {
       galleryRef.innerHTML = "";
       return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
 };
+    galleryRef.innerHTML = "";    
 
     renderImages(data);
+
+    loadMoreBtnRef.addEventListener("click", loadMore);
+
+    function loadMore () {
+      page += 1;
+      fetch(`https://pixabay.com/api/?key=${MY_KEY}&per_page=4&q=${searchRequest}&image_type=photo$orientation=horizontal&safesearch=true&page=${page}`)
+      .then(response => response.json())
+      .then(data => {
+        renderImages(data);
+      })
+      .catch(er => console.log(er.message));
+      };
 })
 .catch(er => console.log(er.message))
 };
 
 function renderImages(data) {
-  galleryRef.innerHTML = "";
-  
+ 
 const markup = data.hits.map(image => 
     `<div class="photo-card">
     <div class="photo-thumb">
@@ -67,6 +72,7 @@ const markup = data.hits.map(image =>
     </div>`
     )
     .join("");
-    console.log(markup);
-    galleryRef.innerHTML = markup;
+    // console.log(markup);
+    galleryRef.insertAdjacentHTML("beforeend", markup);
 };
+
