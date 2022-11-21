30946af71b7700ed166868085d041d7e5164d1bf
Elena Pastushenko <heleora_net@ukr.net>
2022-11-21 18:49:23 +0200

    asunc functions are working
    Needs to add try...catch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
+// import SimpleLightbox from 'simplelightbox';
 
 const searchFormRef = document.querySelector("form");
 const galleryRef = document.querySelector(".gallery");
@@ -7,15 +8,16 @@ const loadMoreBtnRef = document.querySelector(".load-more");
 
 const baseURL = "https://pixabay.com/api/";
 const MY_KEY = "31431755-1c4852ed09ff5890501267879";
-let page = 1;
+let nextPage = 1;
 const perPage = 40;
+const totalPages = 500 / perPage;
 
 searchFormRef.addEventListener("submit", searchImages);
-loadMoreBtnRef.addEventListener("click", loadMore);
 
 function searchImages(evt) {
   evt.preventDefault();
   galleryRef.innerHTML = "";
+  nextPage = 1;
   const searchRequest = evt.currentTarget.elements.searchQuery.value;
   fetchImages(searchRequest);
 };
@@ -28,20 +30,36 @@ const response = await axios.get(`${baseURL}?key=${MY_KEY}&per_page=${perPage}&q
     galleryRef.innerHTML = "";
     return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
 };
+
 const totalPermittedImages = response.data.totalHits;
 Notiflix.Notify.success(`Hooray! We found ${totalPermittedImages} images.`);
 galleryRef.innerHTML = "";
 
 renderImages(data);
+nextPage += 1;
 loadMoreBtnRef.classList.remove("hidden");
+loadMoreBtnRef.addEventListener("click", loadMore);
+
+async function loadMore(){
+  loadMoreBtnRef.classList.add("hidden");
+  console.log("Номер следущей страницы:", nextPage);
+  if (nextPage > totalPages){
+    return Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+  };
+
+  loadMoreBtnRef.classList.remove("hidden");
+  const additionalResponse = await axios.get(`${baseURL}?key=${MY_KEY}&per_page=${perPage}&q=${searchRequest}&image_type=photo$orientation=horizontal&safesearch=true&page=${nextPage}`)
+  .then(additionalResponse => {
+    renderImages(data);
+    loadMoreBtnRef.classList.remove("hidden");
+    nextPage += 1;
+  })
+  };
 })
 };
 
 function renderImages(data) {
-  // numberOfRenderImages += perPage;
-  // console.log(`отрисовано: ${numberOfRenderImages}`);
-  page += 1;
-  const markup = data.map(image => 
+const markup = data.map(image => 
       `<div class="photo-card">
       <div class="photo-thumb">
       <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="500px"/>
@@ -65,9 +83,4 @@ function renderImages(data) {
       .join("");
       // console.log(markup);
       galleryRef.insertAdjacentHTML("beforeend", markup);
-  };
-
-function loadMore(){
-  loadMoreBtnRef.classList.add("hidden");
-  console.log("Номер страницы:", page);
   };
